638b2e999f53ab32b16e2a7a5c8e1cb6ad14fd70
Nithrom <miguel.horda@gmail.com>
2021-03-06 19:14:00 +0100

    Add new page

---
+++ public/trabajo_final/js/js.js
@@ -0,0 +1,64 @@
+var botonNombre=document.getElementById('nombre');
+var formNombre=document.getElementById('formNombre');
+var nombre=document.getElementById('nombreUsuario');
+var parrafo=document.getElementsByClassName('oculto');
+var user=form_nombre.user;
+/* Variables para "registro" de usuario */
+
+var sinopsis=document.getElementsByClassName('sinopOculta sinopsis');
+var verSinopsis=document.getElementsByClassName('leerSinopsis');
+var closeSinopsis=document.getElementsByClassName('cerrarSinopsis');
+
+/* Variables para mostrar y ocultar la sinopsis */
+
+var temas=color.colores;
+var cambioCss=document.getElementsByTagName('link');
+/* Variables para el cambio de tema */
+
+function cambiarTema(css){
+	cambioCss[1].href=css.toString();
+}
+/*Funcion para cambiar el tema */
+
+function mostrarNombre(){
+	if(user.value!=''){
+		formNombre.style.display="none";
+		nombre.innerText=user.value;
+		parrafo[0].style.display="block";
+	}
+	else{
+		formNombre.style.display="inline";
+		user.style.backgroundColor="red";
+		user.placeholder="Campo requerido";
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+	}
+}
+/* Funcion para mostrar el nombre */
+
+function mostrarSinopsis(id){
+	sinopsis[parseInt(id)].style.visibility="visible";
+	sinopsis[parseInt(id)].style.opacity="1";
+}
+/* Funcion para mostrar sinopsis */
+
+function cerrarSinopsis(num){
+	sinopsis[parseInt(num)].style.visibility="hidden";
+	sinopsis[parseInt(num)].style.opacity="0";
+}
+/* Funcion para ocultar la sinopsis */
+
+botonNombre.addEventListener("click",mostrarNombre);
+
+for(var i=0 ;i<temas.length ; i++){
+	temas[i].addEventListener("click",function(){cambiarTema(this.value);});
+}
+
+for(var c=0 ; c<verSinopsis.length ; c++){
+	verSinopsis[c].addEventListener("click",function(){mostrarSinopsis(this.id);});
+}
+
+for(var x=0 ; x<closeSinopsis.length ; x++){
+	closeSinopsis[x].addEventListener("click",function(){cerrarSinopsis(this.value);});
+}
+/* Botones y controladores de eventos */
---
+++ public/trabajo_final/js/recomendar.js
@@ -0,0 +1,131 @@
+var tuyos=document.getElementById('oculto');
+var usuario=recomiendo.usuario;
+var titulo=recomiendo.libro;
+var texto=recomiendo.opinion;
+var enviarLibro=document.getElementById('boton');
+var recomendar=document.getElementById('recomendadosUsuario');
+/* Variables recomendar libro */
+
+var caracteres=document.getElementById('contador');
+var maxCar=500;
+/* Variables contador */
+
+var botonNombre=document.getElementById('nombre');
+var formNombre=document.getElementById('formNombre');
+var nombre=document.getElementById('nombreUsuario');
+var parrafo=document.getElementsByClassName('oculto');
+var user=form_nombre.user;
+/* Variables para "registro" de usuario */
+
+
+function recomendado(){
+	
+	if(usuario.value=='' && titulo.value=='' && texto.value==''){
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+		titulo.style.backgroundColor="red";
+		titulo.placeholder="Campo requerido";
+		texto.style.backgroundColor="red";
+		texto.placeholder="Campo requerido";
+	}
+	else if(usuario.value!='' && titulo.value=='' && texto.value==''){
+		usuario.style.backgroundColor="white";
+		titulo.style.backgroundColor="red";
+		titulo.placeholder="Campo requerido";
+		texto.style.backgroundColor="red";
+		texto.placeholder="Campo requerido";
+	}
+	else if(usuario.value=='' && titulo.value!='' && texto.value==''){
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+		titulo.style.backgroundColor="white";
+		texto.style.backgroundColor="red";
+		texto.placeholder="Campo requerido";
+	}
+	else if(usuario.value=='' && titulo.value=='' && texto.value!=''){
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+		titulo.style.backgroundColor="red";
+		titulo.placeholder="Campo requerido";
+	}
+	else if(usuario.value!='' && titulo.value!='' && texto.value==''){
+		usuario.style.backgroundColor="white";
+		titulo.style.backgroundColor="white";
+		texto.style.backgroundColor="red";
+	}
+	else if(usuario.value!='' && titulo.value=='' && texto.value!=''){
+		usuario.style.backgroundColor="white";
+		titulo.style.backgroundColor="red";
+		texto.style.backgroundColor="white";
+	}
+	else if(usuario.value=='' && titulo.value!='' && texto.value!=''){
+		usuario.style.backgroundColor="red";
+		texto.style.backgroundColor="white";
+		titulo.style.backgroundColor="white";
+	}
+	else if(usuario.value==''){
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+	}
+	else if(titulo.value==''){
+		titulo.style.backgroundColor="red";
+		titulo.placeholder="Campo requerido";
+	}
+	else if(texto.value==''){
+		texto.style.backgroundColor="red";
+		texto.placeholder="Campo requerido";
+	}
+	else{
+		tuyos.style.display="block";
+		recomendar.innerHTML+='<img src="../media/img/usuario.png" alt="usuario"/><span>'+usuario.value+'</span><h3>Libro recomendado: '+titulo.value+'</h3><b>SINOPSIS:</b><p>'+texto.value+'</p>';
+	}
+	if(user.value==''){
+		user.value=usuario.value;
+	}
+}
+
+function mostrarNombre(){
+	if(user.value!='' && usuario.value==''){
+		formNombre.style.display="none";
+		nombre.innerText=user.value;
+		usuario.value=user.value;
+		parrafo[0].style.display="inline";
+	}
+	else if(user.value=='' && usuario.value!=''){
+		formNombre.style.display="none";
+		nombre.innerText=usuario.value;
+		parrafo[0].style.display="inline";
+	}
+	else if(usuario.value!='', user.value!=''){
+		formNombre.style.display="none";
+		nombre.innerText=user.value;
+		parrafo[0].style.display="inline";
+	}
+	else{
+		formNombre.style.display="inline";
+		user.style.backgroundColor="red";
+		user.placeholder="Campo requerido";
+		usuario.style.backgroundColor="red";
+		usuario.placeholder="Campo requerido";
+	}
+}
+/* Funcion para mostrar el nombre */
+
+function contador(cadena){
+	caracteres.innerText=cadena.length;
+	if(cadena.length==maxCar){
+		max.style.color="red";
+	}
+	else{
+		max.style.color="black";
+	}
+}
+/* Funcion para limite de caracteres */
+
+botonNombre.addEventListener("click",mostrarNombre);
+
+texto.addEventListener("keyup",function(){contador(this.value);});
+
+texto.addEventListener("keydown",function(){contador(this.value);});
+
+enviarLibro.addEventListener("click",recomendado);
