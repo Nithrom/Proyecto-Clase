dc994450238e2d030562f8a2f548fe1c7e5b5481
Nithrom <miguel.horda@gmail.com>
2021-03-06 23:29:21 +0100

    Small changes

--- proyectos/index_css/js/js.js
+++ proyectos/index_css/js/js.js
@@ -1,24 +1,29 @@
 var list=document.getElementsByTagName('li');
 var imgSpace=document.getElementById('img');
+
 function showImg(img){
 	if(img=='img1'){
 		imgSpace.src='index_css/img/face.png';
+		imgSpace.className='aparece';
 	}
 	else if(img=='img2'){
 		imgSpace.src='index_css/img/cosmere.png';
+		imgSpace.className='aparece';
 	}
 	else if(img=='img3'){
 		imgSpace.src='index_css/img/taller.png';
+		imgSpace.className='aparece';
 	}
 	else{
 		imgSpace.src='index_css/img/libreria.png';
+		imgSpace.className='aparece';
 	}
-
 }
+
 function ocultImg(){
-	imgSpace.src='';
+	imgSpace.className='desaparece';
 }
 for(var i=0;i<list.length;i++){
 	list[i].addEventListener("mouseenter",function(){showImg(this.id)});
-	list[i].addEventListener("mouseleave",ocultImg);
+	list[i].addEventListener("mouseleave",function(){ocultImg(this.id)});
 }
